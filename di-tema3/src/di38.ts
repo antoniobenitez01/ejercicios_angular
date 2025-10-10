6b198fd29edc76d22e34b187aefc74d726f45b71
antoniobenitez01 <abenrod@g.educaand.es>
2025-10-10 19:50:17 +0200

    Ejercicio 3.8

--- di-tema3/src/di38.ts
+++ di-tema3/src/di38.ts
@@ -1,23 +1,29 @@
+/*
+ *  === DESESTRUCTURACIÓN DE ARGUMENTOS ===
+ *  En este proyecto, vamos a crear diferentes interfaces y funciones
+ *  y vamos a acceder a diferentes datos mediante el uso de la desestructuración en argumentos
+*/
+// PRODUCTO - Representa un producto con su descripción y precio
 interface Producto {
-    descripcion: string;
-    precio: number;
+    descripcion: string;                // Descripcion
+    precio: number;                     // Precio
 }
 
-const telefono: Producto = {
+const telefono: Producto = { // Creamos objeto Producto "telefono"
     descripcion: "iPhone 15 Pro",
     precio: 1000
 }
 
-const tablet: Producto = {
+const tablet: Producto = { // Creamos objeto Producto "tablet"
     descripcion: "iPad Air 3",
     precio: 500
 }
-
+// CALCULA IMPUESTO OPCIONES - Opciones para calcular el impuesto de varios objetos Producto
 interface CalculaImpuestoOpciones {
     impuesto: number;
     productos: Producto[];
 }
-
+// -- CALCULA IMPUESTO - Devuelve el impuesto de cada producto en un objeto CalculaImpuestoOpciones
 function calculaImpuesto (opciones: CalculaImpuestoOpciones): number[] {
     let total = 0;
     opciones.productos.forEach(producto => {
@@ -26,17 +32,28 @@ function calculaImpuesto (opciones: CalculaImpuestoOpciones): number[] {
     return [total, total * opciones.impuesto];
 }
 
-const carritoCompra = [telefono, tablet];
-const impuesto = 0.21;
+const carritoCompra = [telefono, tablet]; // Array de objetos Producto
+const impuesto = 0.21;                    // Variable impuesto 
 
-const resultado = calculaImpuesto({
-    productos: carritoCompra,
-    impuesto: impuesto
-    // También podemos poner por la línea anterior, ya que se llaman iguales: impuesto
-})
+// DESESTRUCTURACIÓN DE ARGUMENTOS - A continuación vamos a realizar tres tipos diferentes de desestructuración de argumentos
+console.log("=== DESESTRUCTURACIÓN DE ARGUMENTOS ===");
 
-console.log("Total",resultado[0]);
-console.log("Impuesto",resultado[1]);
+// * 1. Cuando recibimos parámetros en las funciones ----------------------------------------------
+const resultado1 = calculaImpuesto({impuesto: impuesto, productos: carritoCompra});
+console.log("1. Resultado función =",resultado1);
+
+// * 2. Cuando creamos una variable a partir de una función  --------------------------------------
+// -- CREAR PRODUCTO - Función que crea un objeto de Producto
+function crearProducto (): Producto {
+    return {descripcion: "Balón",precio: 100};
+}
+const {descripcion, precio} = crearProducto();
+console.log("2. Descripción =",descripcion,"- Precio =",precio);
+
+// * 3. Cuando vamos a acceder a un dato en alguna estructura de control --------------------------
+const [dato1] = calculaImpuesto({impuesto: impuesto, productos: carritoCompra});
+const [,dato2] = calculaImpuesto({impuesto: impuesto, productos: carritoCompra});
+console.log("3. Primer dato Array =",dato1,"- Segundo dato Array =",dato2);
 
 export{};
 
